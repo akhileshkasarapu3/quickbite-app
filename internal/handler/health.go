f9a81ae6b08e91afe4fac22f55f9ab1cf7a0afec
kakhileshkrishna <akhileshkrishnakasarapu@gmail.com>
2026-03-22 19:48:26 -0500

    Use struct for health JSON response

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -5,19 +5,25 @@ import (
 	"encoding/json"
 )
 
+type HealthResponse struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+
 func HealthHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusOK)		// Set http status code 200
 
-	response := map[string]string {
-		"status": "success",
-		"message": "Quickbite is Running Successfully!",
+	response := HealthResponse{
+		Status : "Success",
+		Message : "Quick Bite is running",
 	}
 
 	// converts the Go map into JSON and writes it into the response.
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		http.Error(w, "Failed to Encode response", http.StatusInternalServerError)
+		return 
 	}
 }
