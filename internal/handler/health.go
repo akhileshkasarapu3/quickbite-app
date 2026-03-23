a69c245577a0d3d4d52945ac6d6961493a3e7d77
kakhileshkrishna <akhileshkrishnakasarapu@gmail.com>
2026-03-22 22:26:08 -0500

    Add WriteError and WriteSuccess response helpers

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -6,31 +6,17 @@ import (
 	"github.com/akhileshkasarapu3/quickbite/internal/response"
 )
 
-type HealthResponse struct {
-	Status string `json:"status"`
-	Message string `json:"message"`
-}
-
-type ErrorResponse struct {
-	Status string `json:"status"`
+type HealthData struct {
 	Message string `json:"message"`
 }
 
 func HealthHandler(w http.ResponseWriter, r *http.Request){
 	if r.Method != http.MethodGet {
-		errorResponse := ErrorResponse{
-			Status: "error",
-			Message: "method not allowed",
-		}
-
-		response.WriteJson(w, http.StatusMethodNotAllowed, errorResponse)
-		return
+		response.WriteError(w, http.StatusMethodNotAllowed, "method not allowed")
 	}
 
-	successResponse := HealthResponse{
-		Status : "Success",
-		Message : "Quick Bite is running",
+	healthCheckData := HealthData{
+		Message: "Quick Bite API is Running",
 	}
-
-	response.WriteJson(w, http.StatusOK, successResponse)
+	response.WriteSuccess(w, http.StatusOK, healthCheckData)
 }
--- internal/response/json.go
+++ internal/response/json.go
@@ -5,6 +5,17 @@ import (
 	"encoding/json"
 )
 
+type ErrorResponse struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+
+type APIResponse struct {
+	Status string `json:"status"`
+	Data 	any 	`json:"data"`
+}
+
+
 func WriteJson(w http.ResponseWriter, statusCode int, data any){
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
@@ -14,4 +25,22 @@ func WriteJson(w http.ResponseWriter, statusCode int, data any){
 		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
 		return
 	}
+}
+
+func WriteSuccess(w http.ResponseWriter, statusCode int, data any){
+	successResponse := APIResponse{
+		Status: "success",
+		Data: data,
+	}
+
+	WriteJson(w, statusCode, successResponse)
+}
+
+func WriteError(w http.ResponseWriter, statusCode int, message string){
+	errorResponse := ErrorResponse{
+		Status: "error",
+		Message: message,
+	}
+
+	WriteJson(w, statusCode, errorResponse)
 }
