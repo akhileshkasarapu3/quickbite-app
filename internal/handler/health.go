2674f7848d682f6dcd806ecdadcf38e407b312ac
kakhileshkrishna <akhileshkrishnakasarapu@gmail.com>
2026-03-22 19:42:18 -0500

    Return JSON response from health endpoint

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -1,10 +1,23 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
 func HealthHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Quick Bite is running")
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)		// Set http status code 200
+
+	response := map[string]string {
+		"status": "success",
+		"message": "Quickbite is Running Successfully!",
+	}
+
+	// converts the Go map into JSON and writes it into the response.
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Failed to Encode response", http.StatusInternalServerError)
+	}
 }
